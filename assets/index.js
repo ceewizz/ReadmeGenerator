0daa31c565428ded12cdb0642e0dd9d0c837417b
ceewizz <ceewizzhuhwin@gmail.com>
2023-12-04 01:49:23 -0800

    updated index.js

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,165 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// directing to where the readme is dev.
+const generatePage = require('.utils/generateMarkdown.js');
+// Create an array of questions for user input
+const questions = () => {
 
-// TODO: Create a function to initialize app
-function init() {}
+    // prompting user for questions using inquirer 
+    return inquirer.prompt([
 
-// Function call to initialize app
-init();
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter github username',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            }
+            else {
+                console.log('Please enter a valid github username!');
+                return false;
+            }
+            }
+        },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email address',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+
+    }
+    else {
+        console.log('Please enter a valid email address!');
+        return false;
+
+    }
+}
+    },
+    {
+        type: 'input',
+        name: 'title    ',
+        message: 'Enter your project title',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+
+    }
+    else {
+        console.log('Please enter a name for your project');
+        return false;
+    }
+}
+    },
+    {
+        type : 'input',
+        name: 'description',
+        message: 'Please enter a short description for your project',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            }
+            else {
+                console.log('Please enter a description for your project');
+                return false;
+            }
+
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What is your license for this project?',
+            choices: ['GNU', 'MIT'],
+            default: ["GNU"],
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a license for this project');
+                    return false;
+
+                }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the requirements to install your project?',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+
+            }
+            else {
+                console.log('Please enter a valid requirement to install your project');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Instructions to use this project',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            }
+            else {
+                console.log('Please enter an instruction to use this project');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Command to test this project',
+        default: 'npm test'
+
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'List of contributors to use this project',
+    }
+
+]);
+};
+
+ 
+
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', data, error => {
+        // Error
+        if(err) {
+            console.log(err);
+            return;
+        }
+        else {
+            console.log('Your README has been successfully created')
+
+        }
+        })
+    };
+    
+   questions()
+   .then(answers => {
+    return generatePage(answers);
+   })
+   .then(data => {
+    return writeFile(data);
+   })
+   .catch(err => {
+    console.log(err)
+})
+
+
